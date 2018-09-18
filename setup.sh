e0d2feb56bb982aada63654fbca64beeebc330b1
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2018-09-18 23:27:28 +0900

    Update message in setup script

--- setup.sh
+++ setup.sh
@@ -12,14 +12,14 @@ for source in $SOURCE_DIR/*; do
 
   if [ -L $symlink_fullpath ]; then
     if [ `readlink $symlink_fullpath` = $original_fullpath ]; then
-      echo "$symlink_fullpath\t=>\t$original_fullpath"
+      echo "Symlink: $symlink_fullpath => $original_fullpath"
     else
-      echo "\033[31mSymlink already exists: $symlink_fullpath\033[m"
+      echo "\033[31mConflicted symlink: $symlink_fullpath\033[m"
     fi
   elif [ -e $symlink_fullpath ]; then
-    echo "\033[31mFile already exists: $symlink_fullpath\033[m"
+    echo "\033[31mConflicted file: $symlink_fullpath\033[m"
   else
-    echo "\033[32m$symlink_fullpath\t=>\t$original_fullpath\033[m"
+    echo "\033[32mSymlink: $symlink_fullpath => $original_fullpath\033[m"
     ln -s $original_fullpath $symlink_fullpath
   fi
 done
