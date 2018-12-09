cc26ea95d9e474dd78d9379353532163e56d9011
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2018-12-09 17:39:33 +0900

    Install fisherman and peco plugin

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,2 +1,9 @@
 # rbenv
 status --is-interactive; and source (rbenv init -|psub)
+
+# Bootstrap installation for fisher
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
